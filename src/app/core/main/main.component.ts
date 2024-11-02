d7ff00e91467df03a586c28b73e4bc71d1c1e239
Claudia Simone De Souza <110047472+ClaudiaSouza1812@users.noreply.github.com>
2024-11-02 15:46:53 +0000

    241102

--- src/app/core/main/main.component.ts
+++ src/app/core/main/main.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CarouselComponent } from "./carousel/carousel.component";
+import { HighlightComponent } from "./highlight/highlight.component";
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [CarouselComponent],
+  imports: [CarouselComponent, HighlightComponent],
   templateUrl: './main.component.html',
   styleUrl: './main.component.css'
 })
