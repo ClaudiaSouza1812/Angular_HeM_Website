cbc0c32c5ca65606488ffc68be314e281463c4ed
Claudia Simone De Souza <110047472+ClaudiaSouza1812@users.noreply.github.com>
2024-11-02 16:40:03 +0000

    241102

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from "./core/header/header.component";
-import { TopmenuComponent } from './core/header/topmenu/topmenu.component';
+import { TopmenuComponent } from './shared/components/topmenu/topmenu.component';
 import { CommonModule } from '@angular/common';
 import { CarouselComponent } from './features/home/components/carousel/carousel.component';
 import { HighlightComponent } from './features/home/components/highlight/highlight.component';
+import { FooterComponent } from "./shared/components/footer/footer.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HeaderComponent, TopmenuComponent, CarouselComponent, HighlightComponent],
+  imports: [CommonModule, RouterOutlet, TopmenuComponent, TopmenuComponent, CarouselComponent, HighlightComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
